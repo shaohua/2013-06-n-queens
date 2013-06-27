416fd3e085efaeb462b34e60b5b6d69f0669bf75
Hack Reactor Students <github@hackreactor.com>
2013-06-27 13:20:40 -0700

    Working walker.js

--- walkers.js
+++ walkers.js
@@ -24,7 +24,7 @@
 // walker(0);
 
 
-var n = 4;
+var n = 3;
 var counter = 0;
 
 var matrix = [
@@ -34,20 +34,19 @@ var matrix = [
 ];
 
 var walker = function(rowIndex){
-  console.log('matrix', matrix);
 
   for(var j = 0; j < n; j++){
-    matrix[rowIndex][j] = 1;
+    matrix[rowIndex][j] = 1; //toggle ON
+    console.log("matrix\n" + matrix.join("\n"));
 
     if(rowIndex + 1 < n){
       walker(rowIndex + 1);
     }
 
-    matrix[rowIndex][j] = 0;
+    matrix[rowIndex][j] = 0; //toggle OFF
 
   }
 
-  console.log();
 };
 
 walker(0);
