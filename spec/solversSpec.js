87605037ce98307453c4aa3ba383311a4ca4cc51
shao <shao@shao.net>
2013-06-27 23:10:52 -0700

    Change n to 9

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -37,7 +37,7 @@ describe("solvers", function() {
 
   describe('countNQueensSolutions()', function(){
 
-    it('finds the number of valid solutions for n of 0-10', function(){
+    it('finds the number of valid solutions for n of 0-9', function(){
       _.range(0, 9).map(function(n){
         var solutionCount = countNQueensSolutions(n);
         var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596][n];
@@ -49,7 +49,7 @@ describe("solvers", function() {
 
   describe('countNQueensSolutionsHash()', function(){
 
-    it('finds the number of valid solutions for n of 0-10', function(){
+    it('finds the number of valid solutions for n of 0-9', function(){
       _.range(0, 9).map(function(n){
         var solutionCount = countNQueensSolutions(n);
         var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596][n];
